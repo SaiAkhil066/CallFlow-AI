b8a7297dca0b7009a81ede37464932fe7feff304
N Sai Akhil <97727609+SaiAkhil066@users.noreply.github.com>
2025-08-20 10:27:25 +0530

    Delete landing_page directory

--- landing_page/api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from landing_app import app
-
-# Vercel serverless function handler
-def handler(request, response):
-    return app(request, response)
